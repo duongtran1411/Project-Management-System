be9fbc9f82c426f3c406a6ddda74dc66f8689242
duongtran1411 <duongtdhe172042@fpt.edu.vn>
2025-06-11 01:45:55 +0700

    init project

--- fe-pms/src/app/layout.tsx
+++ fe-pms/src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import '../styles/globals.css';
 import 'antd/dist/reset.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/reset.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
---
+++ fe-pms/src/lib/endpoints.ts
@@ -0,0 +1,5 @@
+export class Endpoints{
+    static readonly Auth = {
+    REFRESH: "auth/refresh",
+  }
+}
