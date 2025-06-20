ac1a81664c103b52165db08cc16fd8561a4fc098
Nguyễn Thái Sơn <sonnthe172793@fpt.edu.vn>
2025-06-20 23:07:46 +0700

    f

--- be-pms/src/services/auth.service.ts
+++ be-pms/src/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { generateToken, generateRefreshToken } from "./../utils/jwt.util";
 import User, { IUser } from "../models/user.model";
 import jwt from "jsonwebtoken";
 import { verifyGoogleIdToken } from "../utils/google-auth.util";
@@ -21,7 +22,7 @@ export class AuthService {
     const googleUser = await verifyGoogleIdToken(idToken);
     // 2. Tìm user theo email
     let user = await User.findOne({ email: googleUser.email });
-    let roleDefault = await Role.findOne({name : {$eq : "USER"}})
+    let roleDefault = await Role.findOne({ name: { $eq: "USER" } });
     let isNewUser = false;
     let tempPassword = "";
     if (!user) {
@@ -35,7 +36,7 @@ export class AuthService {
         avatar: googleUser.picture,
         status: "ACTIVE",
         verified: true,
-        role: roleDefault?._id
+        role: roleDefault?._id,
       });
       isNewUser = true;
       // 4. Chỉ gửi email mật khẩu khi tạo user mới lần đầu
@@ -52,8 +53,8 @@ export class AuthService {
     user.lastLogin = new Date();
     await user.save();
     // 6. Trả về JWT
-    const access_token = this.generateToken(user);
-    const refresh_token = this.generateRefreshToken(user);
+    const access_token = generateToken(user);
+    const refresh_token = generateRefreshToken(user);
     const { password: _, ...userResponse } = user.toObject();
     return {
       user: userResponse,
@@ -71,7 +72,7 @@ export class AuthService {
         throw new Error("Invalid refresh token");
       }
 
-      const token = this.generateToken(user);
+      const token = generateToken(user);
       return { token };
     } catch (error) {
       throw new Error("Invalid refresh token");
@@ -107,38 +108,14 @@ export class AuthService {
     user.failedLoginAttempts = 0;
     user.lastLogin = new Date();
     await user.save();
-    const access_token = this.generateToken(user);
-    const refresh_token = this.generateRefreshToken(user);
+    const access_token = generateToken(user);
+    const refresh_token = generateRefreshToken(user);
     const { password: _, ...userResponse } = user.toObject();
     return {
       user: userResponse,
       access_token,
       refresh_token,
     };
-  }
-
-  private generateToken(user: IUser): string {
-    return jwt.sign(
-      {
-        userId: (user._id as any).toString(),
-        email: user.email,
-        role: user.role,
-      },
-      JWT_SECRET,
-      { expiresIn: "30d" }
-    );
-  }
-
-  private generateRefreshToken(user: IUser): string {
-    return jwt.sign(
-      {
-        userId: (user._id as any).toString(),
-        email: user.email,
-        role: user.role,
-      },
-      JWT_SECRET,
-      { expiresIn: "90d" }
-    );
   }
 
   async forgotPassword(email: string): Promise<void> {
