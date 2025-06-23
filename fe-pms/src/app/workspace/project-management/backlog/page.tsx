a93fbb4489356eb33030a0f85aa02b60f03c2de3
kimquy <kimquy001623@gmail.com>
2025-06-23 11:31:55 +0700

    UI backlog - milestone screen

--- fe-pms/src/app/workspace/project-management/backlog/page.tsx
+++ fe-pms/src/app/workspace/project-management/backlog/page.tsx
@@ -15,8 +15,11 @@ import {
 import {
   DownOutlined,
   EllipsisOutlined,
+  PlusOutlined,
+  RightOutlined,
   SearchOutlined,
 } from "@ant-design/icons";
+import React, { useState } from "react";
 
 interface DataType {
   key: string;
@@ -139,6 +142,16 @@ const overlayContent = (
     <Checkbox value="Option B">Nguyễn Thái Sơn</Checkbox>
     <Checkbox value="Option C">Hoàng Thị Hương Giang</Checkbox>
     <Checkbox value="Option D">Lê Văn Việt</Checkbox>
+    <Checkbox value="Option D">Unassigned</Checkbox>
+  </div>
+);
+
+const overlayEpic = (
+  <div className="flex flex-col gap-2 bg-white p-4 ml-2 rounded-md shadow-lg">
+    <Checkbox value="Option A">No Epic</Checkbox>
+    <Checkbox value="Option B">SRS Document</Checkbox>
+    <Checkbox value="Option C">Project Tracking</Checkbox>
+    <Checkbox value="Option D">BACKEND API</Checkbox>
   </div>
 );
 
@@ -161,12 +174,16 @@ const items = [
   },
 ];
 export default function Backlog() {
+  const [showTable, setShowTable] = useState<boolean>(true);
   const onChange: CheckboxProps["onChange"] = (e) => {
     console.log(`checked = ${e.target.checked}`);
   };
+  const handleShowTable = () => {
+    setShowTable(!showTable);
+  };
   return (
-    <div className="p-6 bg-white min-h-screen">
-      <div className="flex items-center gap-3 mb-4">
+    <div className="p-6 bg-white min-h-screen ">
+      <div className="flex items-center gap-3 mb-4  top-0 left-0 right-0">
         {/* Search */}
         <Input
           placeholder="Search"
@@ -174,27 +191,32 @@ export default function Backlog() {
           className="bg-gray-100 w-[184px] h-[32px] border-gray-400"
         />
         {/* Search by member */}
-        <Avatar.Group
-          size={30}
-          max={{
-            count: 2,
-            style: { color: "#f56a00", backgroundColor: "#fde3cf" },
-          }}
-        >
-          <Avatar style={{ backgroundColor: "#f56a00" }}>K</Avatar>
-          <Avatar
-            style={{
-              backgroundColor: "#f0f1f3",
-              color: "black",
-              fontSize: "12px",
-            }}
-          >
-            +2
-          </Avatar>
-        </Avatar.Group>
+        <Dropdown overlay={overlayContent} trigger={["click"]}>
+          <div className="cursor-pointer">
+            <Avatar.Group
+              size={30}
+              max={{
+                count: 2,
+                style: { color: "#f56a00", backgroundColor: "#fde3cf" },
+              }}
+            >
+              <Avatar style={{ backgroundColor: "#f56a00" }}>K</Avatar>
+              <Avatar
+                style={{
+                  backgroundColor: "#f0f1f3",
+                  color: "black",
+                  fontSize: "12px",
+                }}
+              >
+                +2
+              </Avatar>
+            </Avatar.Group>
+          </div>
+        </Dropdown>
+
         {/* Search by epic */}
         <div className="flex gap-2">
-          <Dropdown overlay={overlayContent} trigger={["click"]}>
+          <Dropdown overlay={overlayEpic} trigger={["click"]}>
             <Button>
               <Space className="text-gray-700 font-semibold">
                 Epic
@@ -208,11 +230,17 @@ export default function Backlog() {
         </Button>
       </div>
 
-      <div className="m-4">
+      <div className="m-4 bg-gray-100 rounded-sm p-[6px]">
         <div className="flex justify-between items-center bg-gray-100 p-2 rounded-t-md ">
           {/* Left */}
           <div className="flex items-center gap-2">
             <Checkbox onChange={onChange} />
+            {showTable ? (
+              <DownOutlined className="text-sm" onClick={handleShowTable} />
+            ) : (
+              <RightOutlined className="text-sm" onClick={handleShowTable} />
+            )}
+
             <h3 className="font-semibold">SCRUM Sprint 2 </h3>
             <span className="text-sm text-gray-500 ml-2">
               13 Jun – 27 Jun (3 of 30 work items visible)
@@ -246,33 +274,82 @@ export default function Backlog() {
             </Dropdown>
           </div>
         </div>
-        <Table<DataType>
-          columns={columns}
-          dataSource={data}
-          pagination={false}
-          rowKey="key"
-          className="border border-t-0 border-gray-200"
-          size="small"
-        />
 
+        {showTable && (
+          <div>
+            <Table<DataType>
+              columns={columns}
+              dataSource={data}
+              pagination={false}
+              rowKey="key"
+              className="border border-t-0 border-gray-200"
+              size="small"
+            />
+
+            <Button
+              type="text"
+              className="font-semibold text-gray-600 my-1 w-full flex justify-start p-2"
+            >
+              <span>
+                {" "}
+                <PlusOutlined /> Create
+              </span>
+            </Button>
+          </div>
+        )}
+      </div>
+
+      {/* Backlog Section */}
+      <div className="m-4 bg-gray-100 rounded-sm p-[6px]">
+        <div className="flex justify-between items-center  p-2 rounded-t-md ">
+          {/* Left */}
+          <div className="flex items-center gap-2">
+            <Checkbox onChange={onChange} disabled={true} />
+            <DownOutlined className="text-sm" />
+
+            <h3 className="font-semibold">Backlog </h3>
+            <span className="text-sm text-gray-500 ml-2">(0 work items)</span>
+          </div>
+          {/* Right */}
+          <div className="flex items-center gap-2 ">
+            <div className="flex items-center ">
+              <Tag color="default" className="w-6 text-center p-0">
+                0
+              </Tag>
+              <Tag color="blue" className="w-6 text-center p-0">
+                0
+              </Tag>
+              <Tag color="green" className="w-6 text-center p-0">
+                0
+              </Tag>
+            </div>
+            <Button type="default" className="text-gray-600 font-semibold">
+              Create sprint
+            </Button>
+            <Dropdown
+              menu={{ items, onClick: handleMenuClick }}
+              placement="bottomRight"
+              trigger={["click"]}
+            >
+              <Button
+                icon={<EllipsisOutlined />}
+                className="border border-gray-300 shadow-sm"
+              />
+            </Dropdown>
+          </div>
+        </div>
+        <div className="border border-gray-300 p-2 m-2 text-gray-700 border-dashed text-center border-[2px] rounded-sm">
+          Your backlog is empty.
+        </div>
         <Button
           type="text"
           className="font-semibold text-gray-600 my-1 w-full flex justify-start p-2"
         >
-          <span> + Create</span>
+          <span>
+            {" "}
+            <PlusOutlined /> Create
+          </span>
         </Button>
-      </div>
-
-      <div className="m-4">
-        <h3 className="font-semibold bg-gray-100 p-2 rounded-t-md">
-          Backlog (0 work items)
-        </h3>
-        <div className="border border-t-0 border-gray-200 p-4 text-gray-400">
-          Your backlog is empty.
-        </div>
-        <div className="p-2 border border-t-0 border-gray-200">
-          <Button type="link">+ Create</Button>
-        </div>
       </div>
     </div>
   );
