400f12ac67d8a794628d0ee84ec578025bd41e2b
kimquy <kimquy001623@gmail.com>
2025-07-03 07:54:39 +0700

    add epic progress chart fix

--- .history/fe-pms/src/lib/services/epic_20250702221632.ts
+++
@@ -1,9 +0,0 @@
-import axiosService from "./axios.service";
-import { Endpoints } from "../endpoints";
-
-export const fetchEpicByProjectId = async (projectId: string) => {
-  const { data } = await axiosService
-    .getAxiosInstance()
-    .get(Endpoints.Epic.GET_BY_PROJECT(projectId));
-  return data;
-};
