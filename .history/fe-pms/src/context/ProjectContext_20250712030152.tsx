6f067d8502e727477db56f7b7d0cae228f5e62e1
kimquy <kimquy001623@gmail.com>
2025-07-12 18:52:16 +0700

    call api for view all project

--- .history/fe-pms/src/context/ProjectContext_20250712030152.tsx
+++
@@ -1,9 +0,0 @@
-// context/ProjectContext.tsx
-"use client";
-
-import { createContext, useContext } from "react";
-
-export const ProjectContext = createContext<{ projectId: string | null }>({
-  projectId: null,
-});
-export const useProject = () => useContext(ProjectContext);
