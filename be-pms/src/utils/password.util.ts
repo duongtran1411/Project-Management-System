b4196c72619a2aebcbdccef8044863bdcb12153a
duongtran1411 <duongtdhe172042@fpt.edu.vn>
2025-07-25 21:40:32 +0700

    fix

--- be-pms/src/utils/password.util.ts
+++ be-pms/src/utils/password.util.ts
@@ -1,9 +1,30 @@
 import crypto from "crypto";
 
 export function generateRandomPassword(length = 8): string {
-  return crypto
-    .randomBytes(6)
-    .toString("base64")
-    .replace(/[^a-zA-Z0-9]/g, "")
-    .slice(0, length);
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const digits = "0123456789";
+  const all = letters + digits;
+  const password: string[] = [];
+
+
+  const letterIndex = crypto.randomBytes(1)[0] % letters.length;
+  const digitIndex = crypto.randomBytes(1)[0] % digits.length;
+  password.push(letters[letterIndex]);
+  password.push(digits[digitIndex]);
+
+
+  const remaining = length - 2;
+  const bytes = crypto.randomBytes(remaining);
+  for (let i = 0; i < remaining; i++) {
+    password.push(all[bytes[i] % all.length]);
+  }
+
+ 
+  for (let i = password.length - 1; i > 0; i--) {
+    const j = crypto.randomBytes(1)[0] % (i + 1);
+    [password[i], password[j]] = [password[j], password[i]];
+  }
+
+  return password.join("");
 }
+
